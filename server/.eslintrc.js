25d9c500daf13c86964ab6ae66e1bf4357d07d47
Yong Lin <wangyonglin1999@gmail.com>
2020-02-23 22:29:04 -0500

    added base files

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb',
+  ],
+  rules: {
+    'linebreak-style': 'off',
+    'no-unused-vars': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-console': 'off'
+  }
+};
